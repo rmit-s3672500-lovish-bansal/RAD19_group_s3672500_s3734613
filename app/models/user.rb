56aba69cad80d6d9ffe7028104b15c59a15adeae
= <=>
2019-05-25 06:09:53 +0000

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,45 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+  attr_accessor :remember_token
+  has_many :courses
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, uniqueness: { case_sensitive: false },
+                   length: { minimum: 3, maximum: 25 }
+  VALID_RMIT_MAIL_REGEX = /[a-z].[a-z]@rmit.edu.au/i.freeze
+  VALID_PASSWORD_REGEX = /(?=.[a-zA-Z])(?=.[0-9])(?=.*[\W]).{8,}/i.freeze
+  validates :email, uniqueness: { case_sensitive: false },
+                    format: { with: VALID_RMIT_MAIL_REGEX,
+                              message: 'registration only open for RMIT staff' },
+                    length: { minimum: 4, maximum: 70 },
+                    presence: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
-  
-  def User.digest(string)
+  validates :password, format: { with: VALID_PASSWORD_REGEX,
+                                 message: 'must contain at least a lowercase letter,
+                                a uppercase, a digit, a special character and 8+ characters' }
+
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-  
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190525004116) do
+ActiveRecord::Schema.define(version: 20190525052202) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20190525004116) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
